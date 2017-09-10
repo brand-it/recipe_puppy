90d211af50cd801a996b6ae96e18e1583a680835
Brandt Lareau <brandt.lareau@rackspace.com>
2017-09-09 21:32:02 -0500

    Added a controller test to make sure I don't have syntax errors

--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe RecipesController, type: :controller do
-
+  render_views
+  describe 'GET index' do
+    context 'html request' do
+      subject { get :index }
+      it { is_expected.to be_success }
+    end
+    context 'xhr' do
+      subject { get :index, xhr: true }
+      it { is_expected.to be_success }
+    end
+  end
 end
